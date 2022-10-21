443fa90f91e4dde6f3d1592c4d71b326f1fca139
Mireia Maldonado Parra <mirmalparra@gmail.com>
2022-10-21 11:24:42 +0200

    add event

--- prueba-1/js/main.js
+++ prueba-1/js/main.js
@@ -6,45 +6,69 @@ addForm.classList.remove("collapsed");
 
 const catList =  document.querySelector(".js-list");
 
+const cat1Image = 'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
+const cat1Name = 'Anastacio'.toUpperCase();
+const cat1Desc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const cat1Race = 'British Shorthair';
+const cat2Image = 'https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg';
+const cat2Name = 'Fiona'.toUpperCase();
+const cat2Desc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const cat2Race = 'British Shorthair';
+const cat3Image = 'https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg';
+const cat3Name = 'Cielo'.toUpperCase();
+const cat3Desc = 'Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste. Es una maravilla acariciarle!';
+const cat3Race = 'British Shorthair';
+// catList.innerHTML = `<li> ${cat1Image} ${cat1Name} ${cat1Desc} ${cat1Race}</li>`;
+// catList.innerHTML += `<li>${cat2Image} ${cat2Name}} ${cat2Desc} ${cat2Race}</li>`;
+// catList.innerHTML += `<li>${cat3Image} ${cat3Name}} ${cat3Desc} ${cat3Race}</li>`;
 const cat1 = `<li class="card">
 <article>
   <img
     class="card_img"
-    src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg"
+    src="${cat1Image}"
     alt="gatito"
   />
-  <h3 class="card_title">Anastacio</h3>
+  <h3 class="card_title">${cat1Name}</h3>
+  <h4 class="card_race">${cat1Race}</h4>
   <p class="card_description">
-    Ruiseño, juguetón, le guta estar tranquilo y que nadie le
-    moleste. Es una maravilla acariciarle!
+    ${cat1Desc}
   </p>
 </article>
 </li>`;
 const cat2 = `<li class="card">
 <img
   class="card_img"
-  src="https://images.emedicinehealth.com/images/article/main_image/cat-scratch-disease.jpg"
+  src="${cat2Image}"
   alt="gatito"
 />
-<h3 class="card_title">Fiona</h3>
-<h4 class="card_race">British Shorthair</h4>
+<h3 class="card_title">${cat2Name}</h3>
+<h4 class="card_race">${cat2Race}</h4>
 <p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
+  ${cat2Desc}
 </p>
-</li>`
+</li>`;
 const cat3 = `<li class="card">
 <img
   class="card_img"
-  src="https://media-cldnry.s-nbcnews.com/image/upload/t_nbcnews-fp-1200-630,f_auto,q_auto:best/newscms/2019_39/3021711/190923-cat-pet-stock-cs-1052a.jpg"
-  alt="gatito"
+  src="${cat3Image}"
 />
-<h3 class="card_title">Cielo</h3>
-<h4 class="card_race">British Shorthair</h4>
+<h3 class="card_title">${cat3Name}</h3>
+<h4 class="card_race">${cat3Race}</h4>
 <p class="card_description">
-  Ruiseño, juguetón, le guta estar tranquilo y que nadie le moleste.
-  Es una maravilla acariciarle!
+  ${cat3Desc}
 </p>
-</li>`
+</li>`;
 
 catList.innerHTML = cat1 + cat2 + cat3;
+
+const formButton = document.querySelector('.fa-plus-circle');
+const form = document.querySelector('.js-new-form new-form');
+formButton.addEventListener('click', (event)=> {
+  event.preventDefault();
+  if (form.classList.contains('collapsed')) {
+  form.classList.remove('collapsed');
+} else {
+    form.classList.add('collapsed');
+  }
+});
+
